f672c04098c210d9a96ceb64db939c42d4ae5c50
weezway <1829207+blueway@users.noreply.github.com>
2020-09-26 17:37:16 +0800

    Update build_release.sh

--- tools/build_release.sh
+++ tools/build_release.sh
@@ -23,7 +23,10 @@ function configure {
 }
 
 function build {
+   sudo apt-get update
    sudo apt-get install gcc-multilib
+   sudo ln -s /usr/lib/x86_64-linux-gnu /usr/lib64
+
   dub build -b release --verror --arch=x86
   strip "$target_path"
 }
